867248044548cf7b74499da196b261cb4ca2d8b9
Douglas Rodrigues <60992950+DouglasRodriguesBoeno@users.noreply.github.com>
2026-04-20 17:40:03 -0300

    fix: temporary exclude

--- src/main/java/brest_cancer/api/BrestCancerApplication.java
+++ src/main/java/brest_cancer/api/BrestCancerApplication.java
@@ -2,12 +2,20 @@ package brest_cancer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class,
+                FlywayAutoConfiguration.class
+        }
+)
 public class BrestCancerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BrestCancerApplication.class, args);
     }
-
 }
