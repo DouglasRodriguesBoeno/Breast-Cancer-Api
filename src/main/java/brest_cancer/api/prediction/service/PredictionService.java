34f20d08c5ce37017e73d4f0c868d47cfb31c033
Douglas Rodrigues <60992950+DouglasRodriguesBoeno@users.noreply.github.com>
2026-04-29 17:07:39 -0300

    feat: data save in db

--- src/main/java/brest_cancer/api/prediction/service/PredictionService.java
+++ src/main/java/brest_cancer/api/prediction/service/PredictionService.java
@@ -5,21 +5,50 @@ import brest_cancer.api.ml.dto.MlPredictRequest;
 import brest_cancer.api.ml.dto.MlPredictResponse;
 import brest_cancer.api.prediction.dto.PredictionRequest;
 import brest_cancer.api.prediction.dto.PredictionResponse;
+import brest_cancer.api.prediction.persistence.entity.PredictionRecord;
+import brest_cancer.api.prediction.persistence.repository.PredictionRecordRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PredictionService {
 
     private final MlServiceClient mlServiceClient;
+    private final PredictionRecordRepository predictionRecordRepository;
 
-    public PredictionService(MlServiceClient mlServiceClient) {
+    public PredictionService(
+            MlServiceClient mlServiceClient,
+            PredictionRecordRepository predictionRecordRepository
+    ) {
         this.mlServiceClient = mlServiceClient;
+        this.predictionRecordRepository = predictionRecordRepository;
     }
 
+    @Transactional
     public PredictionResponse createPrediction(PredictionRequest request) {
         MlPredictResponse mlResponse = mlServiceClient.predict(
                 new MlPredictRequest(request.features())
         );
+
+        PredictionRecord predictionRecord = new PredictionRecord();
+        predictionRecord.setInputFeatures(request.features());
+        predictionRecord.setPredictedLabel(mlResponse.predictedLabel());
+        predictionRecord.setPredictedLabelName(mlResponse.predictedLabelName());
+        predictionRecord.setProbabilityMalignant(mlResponse.probabilityMalignant());
+        predictionRecord.setProbabilityBenign(mlResponse.probabilityBenign());
+        predictionRecord.setUsedThresholdMalignant(mlResponse.usedThresholdMalignant());
+        predictionRecord.setModelType(mlResponse.modelType());
+        predictionRecord.setRiskBand(mlResponse.riskBand());
+        predictionRecord.setSummary(mlResponse.summary());
+        predictionRecord.setConfidenceNote(mlResponse.confidenceNote());
+        predictionRecord.setInputQualityNote(mlResponse.inputQualityNote());
+        predictionRecord.setClinicalNote(mlResponse.clinicalNote());
+        predictionRecord.setUsedFeatures(mlResponse.usedFeatures());
+        predictionRecord.setIgnoredFeatures(mlResponse.ignoredFeatures());
+        predictionRecord.setImputedFeatures(mlResponse.imputedFeatures());
+        predictionRecord.setWarnings(mlResponse.warnings());
+
+        predictionRecordRepository.save(predictionRecord);
 
         return PredictionResponse.from(mlResponse);
     }
