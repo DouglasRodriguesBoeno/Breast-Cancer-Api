20296c65a88b6dcb13ebb530ecd2ecf23c9dbefb
Douglas Rodrigues <60992950+DouglasRodriguesBoeno@users.noreply.github.com>
2026-05-30 14:07:23 -0300

    chore: enable report intelligence configuration properties

--- src/main/java/brest_cancer/api/BrestCancerApplication.java
+++ src/main/java/brest_cancer/api/BrestCancerApplication.java
@@ -1,8 +1,11 @@
 package brest_cancer.api;
 
+import brest_cancer.api.report_intelligence.config.ReportIntelligenceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ReportIntelligenceProperties.class)
 @SpringBootApplication
 public class BrestCancerApplication {
 
