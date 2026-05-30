da03b07016fd35215ca4ca056cd8a6e828cc1e1b
Douglas Rodrigues <60992950+DouglasRodriguesBoeno@users.noreply.github.com>
2026-05-30 15:10:08 -0300

    fix: use supported JsonProperty import for report response

--- src/main/java/brest_cancer/api/report_intelligence/dto/ReportAnalysisResponse.java
+++ src/main/java/brest_cancer/api/report_intelligence/dto/ReportAnalysisResponse.java
@@ -1,7 +1,7 @@
 package brest_cancer.api.report_intelligence.dto;
 
 import brest_cancer.api.report_intelligence.persistence.entity.ReportAnalysisRecord;
-import tools.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.OffsetDateTime;
 import java.util.List;
